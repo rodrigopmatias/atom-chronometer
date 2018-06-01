6f1b61e33261c65d8d7f2d3d92d888deec0d3e4b
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2018-06-01 02:13:46 -0300

    adequations for eslinter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "extends": "airbnb"
+  "env": {
+    "es6": true,
+    "browser": true
+  },
+  "globals": {
+    "atom": true
+  },
+  "extends": "airbnb",
+  "rules": {
+    "no-unused-expressions": ["error", {
+      "allowShortCircuit": true,
+      "allowTernary": true
+    }],
+    "import/extensions": ["error", "ignorePackages"],
+    "import/no-unresolved": ["error", {"ignore": ["atom"]}],
+    "max-len": ["error", {"code": 160}],
+    "no-underscore-dangle": ["error", {
+      "allowAfterThis": true,
+      "allowAfterSuper": true
+    }],
+    "function-paren-newline": "off",
+    "no-param-reassign": "off",
+    "comma-dangle": "off",
+    "no-else-return": "off",
+    "no-console": "off",
+    "class-methods-use-this": "off"
+  }
 };
